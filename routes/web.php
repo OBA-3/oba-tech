dfbab381e744d7cfb29b2171310ac4be2aa2a5b8
yassin6-12 <yassineclasse98@gmail.com>
2026-09-11 19:04:49 +0100

    all project

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,36 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\Admin\ProjectController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\Admin\ProjectController as AdminProjectController;
+
 
 Route::get('/', [HomeController::class, 'index'])
     ->name('home');
 
+
+/*
+|--------------------------------------------------------------------------
+| Public Projects
+|--------------------------------------------------------------------------
+*/
+
 Route::get('/projects/{slug}', [ProjectController::class, 'show'])
     ->name('projects.show');
+
+
+/*
+|--------------------------------------------------------------------------
+| Admin
+|--------------------------------------------------------------------------
+*/
 
 Route::get('/admin', function () {
     return view('admin.dashboard');
 });
 
 
-Route::get('/admin/projects', [ProjectController::class, 'index'])
+Route::get('/admin/projects', [AdminProjectController::class, 'index'])
     ->name('admin.projects.index');
